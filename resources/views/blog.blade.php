c578fcd68d989927fa0b41e54d3f3e667f6bab4a
matien19 <matienkasa@gmail.com>
2024-11-30 22:47:01 +0700

    component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Blog</title>
-</head>
-<body>
-    <h1>hal blog</h1>
-    <article>
-        <h1>judul</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsa quam cumque consequatur! Fugiat similique in quis iusto at. Sed aliquam perspiciatis dolorem pariatur assumenda alias unde saepe fuga maxime?</p>
-    </article>
-    <article>
-        <h1>judul</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ipsa quam cumque consequatur! Fugiat similique in quis iusto at. Sed aliquam perspiciatis dolorem pariatur assumenda alias unde saepe fuga maxime?</p>
-    </article>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini Halaman Bolg</h3>
+</x-layout>
